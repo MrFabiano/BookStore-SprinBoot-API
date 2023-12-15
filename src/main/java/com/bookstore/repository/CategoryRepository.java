1f7510e3357d93d2de1c82902de8e078fc602ccf
Fabiano <fabiannosanttos@gmail.com>
2023-12-15 15:08:02 -0300

    create repository, service

---
+++ src/main/java/com/bookstore/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.repository;
+
+import com.bookstore.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
+
---
+++ src/main/java/com/bookstore/service/DBService.java
@@ -0,0 +1,38 @@
+package com.bookstore.service;
+
+import com.bookstore.domain.Book;
+import com.bookstore.domain.Category;
+import com.bookstore.repository.BookRepository;
+import com.bookstore.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public void instanciaBancoDados(){
+        Category category = new Category(null, "Computing", "Book T.I");
+        Category category1 = new Category(null, "Computing", "Books");
+        Category category2 = new Category(null, "Computing", "Books face");
+
+        Book book = new Book(null, "Clean code", "Teixeira", category);
+        Book book1 = new Book(null, "Book Java", "Java Records", category);
+        Book book2 = new Book(null, "Clean Angular", "Angular Records", category);
+
+        category.getBooks().add(book);
+        category1.getBooks().addAll(List.of(book1, book2));
+
+        categoryRepository.saveAll(List.of(category,category1, category2));
+        bookRepository.saveAll(List.of(book, book1, book2));
+
+
+    }
+}
